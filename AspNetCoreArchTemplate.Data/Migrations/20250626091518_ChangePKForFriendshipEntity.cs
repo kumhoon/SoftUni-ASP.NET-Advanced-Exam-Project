42cbf5e377cf7eb4555f00fd949fb605cc7e20f4
kumhoon <tsvetomir.bratoev@gmail.com>
2025-06-26 12:46:29 +0300

    Basic HTML for Home page added, updated database with new migration

---
+++ AspNetCoreArchTemplate.Data/Migrations/20250626091518_ChangePKForFriendshipEntity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreArchTemplate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePKForFriendshipEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Friendships");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships",
+                columns: new[] { "RequesterId", "AddresseeId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Friendships",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships",
+                column: "Id");
+        }
+    }
+}
